85bf0cb0789404a71f0d1be716ef32fc42f3cbf9
Hlib <pavlikgo78@gmail.com>
2024-11-14 16:48:06 +0200

    feat: add user config entity
    - add reply language enum

---
+++ src/DjinniAIReplyBot.Domain/Entities/UserConfiguration.cs
@@ -0,0 +1,12 @@
+using DjinniAIReplyBot.Domain.Enums;
+
+namespace DjinniAIReplyBot.Domain.Entities;
+
+public class UserConfiguration
+{
+    public long ChatId { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public ReplyLanguage ReplyLanguage { get; set; }
+    public string ParsedResume { get; set; } = string.Empty;
+    public string? AdditionalConfiguration { get; set; }
+}
