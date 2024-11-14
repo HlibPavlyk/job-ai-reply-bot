ce4202c932ab0ee03a2b39c8afadcd85ac1b3e0c
Hlib <pavlikgo78@gmail.com>
2024-11-15 00:53:30 +0200

    feat: connect db in other commands

--- src/DjinniAIReplyBot.Application/Commands/LanguageCommand.cs
+++ src/DjinniAIReplyBot.Application/Commands/LanguageCommand.cs
@@ -1,36 +1,30 @@
-using DjinniAIReplyBot.Application.Abstractions.ExternalServices;
-using DjinniAIReplyBot.Application.Abstractions.Repositories;
 using DjinniAIReplyBot.Application.Abstractions.Telegram;
+using DjinniAIReplyBot.Domain.Enums;
 using DjinniAIReplyBot.Domain.Exceptions;
-using Microsoft.Extensions.DependencyInjection;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace DjinniAIReplyBot.Application.Commands;
 
-public class LanguageCommand : ICommand, IListener
+public class LanguageCommand : BaseCommand, IListener
 {
-    public string Name => "/language";
-    private readonly ITelegramService _client;
+    public override string Name => "/language";
     private readonly ICommandListenerManager _listenerManager;
-    private readonly IUserConfigurationRepository _userConfigurationRepository;
     private readonly Dictionary<long, string> _userLanguages;
 
-    public LanguageCommand(IServiceProvider serviceProvider, ICommandListenerManager listenerManager)
+    public LanguageCommand(IServiceProvider serviceProvider, ICommandListenerManager listenerManager) : base(serviceProvider)
     {
-        using var scope = serviceProvider.CreateScope();
-        _client = scope.ServiceProvider.GetRequiredService<ITelegramService>();
-        _userConfigurationRepository = scope.ServiceProvider.GetRequiredService<IUserConfigurationRepository>();
-        
         _listenerManager = listenerManager;
         _userLanguages = new Dictionary<long, string>();
     }
 
-    public async Task Execute(Update update)
+    public override async Task Execute(Update update)
     {
         if (update.Message?.Text == null) return;
 
         long chatId = update.Message.Chat.Id;
+        await ValidateUserAccess(chatId);
+        
         _listenerManager.StartListen(this, chatId);
 
         _userLanguages[chatId] = string.Empty;
@@ -40,7 +34,7 @@ public class LanguageCommand : ICommand, IListener
             [InlineKeyboardButton.WithCallbackData("Ukrainian", "lang_ua")]
         ]);
 
-        await _client.SendMessageAsync(chatId, "Choose reply generation language:", languageKeyboard);
+        await Client.SendMessageAsync(chatId, "Choose reply generation language:", languageKeyboard);
     }
 
     public async Task GetUpdate(Update update)
@@ -66,9 +60,20 @@ public class LanguageCommand : ICommand, IListener
 
             _userLanguages[chatId] = callbackQuery.Data == "lang_en" ? "English" : "Ukrainian";
 
-            await _client.DeleteMessageAsync(chatId, callbackQuery.Message.MessageId);
-            await _client.SendMessageAsync(chatId, $"Chosen language: {_userLanguages[chatId]}");
+            await Client.DeleteMessageAsync(chatId, callbackQuery.Message.MessageId);
+            await Client.SendMessageAsync(chatId, $"Chosen language: {_userLanguages[chatId]}");
 
+            await ScopedAccessor.UseUserConfigurationRepositoryAsync(async repository =>
+            {
+                var userConfiguration = await repository.GetUserConfigurationAsync(chatId);
+                if(userConfiguration == null)
+                    return;
+                
+                userConfiguration.ReplyLanguage = _userLanguages[chatId] == "English" ? ReplyLanguage.En : ReplyLanguage.Ua;
+                await repository.UpdateUserConfigurationAsync(userConfiguration);
+                await repository.SaveChangesAsync();
+            });
+            
             _userLanguages.Remove(chatId);
             _listenerManager.StopListen(chatId);
         }
