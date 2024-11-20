86a60155a21af87f6b7c29b17bc8b95adf69ee62
Hlib <pavlikgo78@gmail.com>
2024-11-20 23:58:24 +0200

    chore: update telegram service
    - update related interface

--- src/DjinniAIReplyBot.Application/Abstractions/ExternalServices/ITelegramService.cs
+++ src/DjinniAIReplyBot.Application/Abstractions/ExternalServices/ITelegramService.cs
@@ -1,11 +1,13 @@
+using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace DjinniAIReplyBot.Application.Abstractions.ExternalServices;
 
 public interface ITelegramService
 {
-    Task SendMessageAsync(long chatId, string message);
+    Task SendMessageAsync(long chatId, string message,  ParseMode parseMode = ParseMode.None);
     Task SendMessageAsync(long chatId, string message, InlineKeyboardMarkup replyMarkup);
     Task EditMessageReplyMarkupAsync(long chatId, int messageId, InlineKeyboardMarkup? replyMarkup = null);
     Task DeleteMessageAsync(long chatId, int messageId);
+    Task<byte[]> GetDocumentAsync(string fileId);
 }
--- src/DjinniAIReplyBot.Infrastructure/ExternalServices/TelegramService.cs
+++ src/DjinniAIReplyBot.Infrastructure/ExternalServices/TelegramService.cs
@@ -2,6 +2,7 @@ using DjinniAIReplyBot.Application.Abstractions.ExternalServices;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace DjinniAIReplyBot.Infrastructure.ExternalServices;
@@ -9,6 +10,7 @@ namespace DjinniAIReplyBot.Infrastructure.ExternalServices;
 public class TelegramService : ITelegramService
 {
     private readonly TelegramBotClient _bot;
+    private readonly string _telegramBotToken;
 
     public TelegramService(IServiceProvider serviceProvider)
     {
@@ -19,13 +21,14 @@ public class TelegramService : ITelegramService
         {
             throw new InvalidOperationException("Telegram bot token is not configured.");
         }
-        _bot = new TelegramBotClient(token);
+        _telegramBotToken = token;
+        _bot = new TelegramBotClient(_telegramBotToken);
         
     }
 
-    public async Task SendMessageAsync(long chatId, string message)
+    public async Task SendMessageAsync(long chatId, string message, ParseMode parseMode = ParseMode.None)
     {
-        await _bot.SendMessage(chatId, message);
+        await _bot.SendMessage(chatId, message, parseMode);
     }
 
     public async Task SendMessageAsync(long chatId, string message, InlineKeyboardMarkup replyMarkup)
@@ -42,4 +45,18 @@ public class TelegramService : ITelegramService
     {
         await _bot.DeleteMessage(chatId, messageId);
     }
+    
+    public async Task<byte[]> GetDocumentAsync(string fileId)
+    {
+        var file = await _bot.GetFile(fileId);
+
+        var fileUrl = $"https://api.telegram.org/file/bot{_telegramBotToken}/{file.FilePath}";
+
+        using var httpClient = new HttpClient();
+    
+        var fileBytes = await httpClient.GetByteArrayAsync(fileUrl);
+
+        return fileBytes;
+    }
+
 }
